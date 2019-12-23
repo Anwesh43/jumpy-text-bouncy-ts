05f2129dd86953705aa110a6049067de9ca11a97
Anwesh43 <anweshthecool0@gmail.com>
2019-12-23 18:50:24 +0530

    adding constants

---
+++ index.ts
@@ -0,0 +1,8 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight
+const scGap : number = 0.005
+const text : string = "hello"
+const nodes : number = 1
+const foreColor : string = "teal"
+const backColor : string = "#bdbdbd"
+const delay : number = 30
